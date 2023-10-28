ea9ccb57cc71884784b88f2fb79751b6865b76e0
Kushagra <kushagravats911@gmail.com>
2023-10-28 23:21:01 +0530

    Variable & Data Types

---
+++ Tut3.java
@@ -0,0 +1,15 @@
+
+//Variables and Data types in Java programming 
+// 2 Types --> Primitive(Intrinsic) and non primitive(Derieved) data types
+//Primitive DT -->byte,int,float,long,double,char,bool,short.
+public class Tut3 {
+
+  public static void main(String[] args) {
+    int num1=2;
+    int num2=4;
+    int num3=6;
+    int sum=num1+num2+num3;
+    System.out.println("The sum of these numbers are ");
+    System.out.printf("%d",sum);
+  }  
+}
