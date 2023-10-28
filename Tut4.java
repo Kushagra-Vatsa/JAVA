70848efd669fcf441beb0e4f880096d445eb6090
Kushagra <kushagravats911@gmail.com>
2023-10-28 23:36:33 +0530

    Literals

---
+++ Tut4.java
@@ -0,0 +1,21 @@
+//Literals: A constant value which can be assigned to variable in java  
+
+public class Tut4 {
+    
+        public static void main(String[] args) {
+            byte age = 34;
+            int age2 = 56;
+            short age3 = 87;
+            long ageDino = 5666666666666L;
+            char ch = 'A';
+            float f1 = 5.6f;
+            double d1 = 4.66;
+    
+            boolean a = true;
+            System.out.print(age);
+            String str = "Kushagra";
+            System.out.println(str);
+    
+        }
+    
+}
