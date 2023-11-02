67f015cff1d3dc0cd755b945dc4df7b844488c1b
Kushagra <kushagravats911@gmail.com>
2023-11-02 15:16:40 +0530

    Practice Set

---
+++ tut7.java
@@ -0,0 +1,38 @@
+//WAP in java to find the sum of 3 numbers
+// import java.util.Scanner;
+// public class tut7 {
+//     public static void main(String[] args) {
+//         System.out.println("Program to find the sum of 3 numbers ");
+//             Scanner sc=new Scanner(System.in);
+//             System.out.print("Enter the number 1 :");
+//             int a=sc.nextInt();
+//             System.out.print("Enter the number 2 :");
+//             int b=sc.nextInt();
+//             System.out.print("Enter the number 3 :");
+//             int c=sc.nextInt();
+//             int sum=(float)a+b+c;
+//             System.out.println("Sum of the numbers are : "+sum);
+//     }
+// }
+
+
+//WAP in java to calculate cgpa
+/**
+ * tut7
+ */
+// import java.util.Scanner;
+// public class tut7 {
+
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         System.out.println("Enter the marks obtained in subject 1");
+//         int a=sc.nextInt();
+//         System.out.println("Enter the marks obtained in subject 2");
+//         int b=sc.nextInt();
+//         System.out.println("Enter the marks obtained in subject 3");
+//         int c=sc.nextInt();
+//         float cgpa=(a+b+c)/30f;
+//         System.out.println("The cgpa obtained is "+cgpa);
+//     }
+// }
+
