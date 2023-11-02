46b7623042d77d601051538be6670416a6d4a1c7
Kushagra <kushagravats911@gmail.com>
2023-11-02 23:19:00 +0530

    Chapter 2 : Practice Set

---
+++ tut11.java
@@ -0,0 +1,55 @@
+// public class tut11 {
+//     public static void main(String[] args) {
+//         float a=7/4*9/2;
+//         System.out.println(a);//The ans is 4 because 7/4 = 1 and 9/2 =4 since we are performing
+//         // division on integers 
+//         float b=7/4f*9/2f;
+//         System.out.println(b);
+//     }
+// }
+
+// public class tut11 {
+
+//     public static void main(String[] args) {
+//         char grade='B';
+//         grade =(char)(grade + 8);
+//         //We can also write it as grade +=8;
+//         System.out.println(grade);
+//         grade=(char)(grade-8);
+//         //We can also make it as grade-=8;
+//         System.out.println(grade);
+//     }
+// }
+
+
+// import java.util.Scanner;
+// public class tut11 {
+//     public static void main(String[] args) {
+//         int NUM=100;
+//         Scanner sc=new Scanner(System.in);
+//         System.out.println("Enter the number ");
+//         int num=sc.nextInt();
+//         if (num > NUM) {
+//             System.out.println("User entered number is greater than NUM");
+//         }
+//         else
+//         {
+//             System.out.println("NUM is greater than user entered number");
+//         }
+//     }
+    
+// }
+
+/**
+ * tut11
+ */
+// public class tut11 {
+
+//     public static void main(String[] args) {
+//         int v=8,u=4,a=1,s=3;
+//         int y=(v*v-u*u)/(2*a*s);
+//         System.out.println(y);
+//         int z=7*49/7+35/7;
+//         System.out.println(z);
+//     }
+// }
