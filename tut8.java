46edec3d0cfb29279be5249f4206373752f732b6
Kushagra <kushagravats911@gmail.com>
2023-11-02 15:39:16 +0530

    Operators

---
+++ tut8.java
@@ -0,0 +1,13 @@
+//Operators are used to perform operation on values and variables .
+// Types of Operators
+// 1:Arithmetic Operators
+// 2:Assignment Operators
+// 3:Comparision Operators
+// 4:Logical Operators
+// 5:Bitwise Operators 
+
+public class tut8 {
+    public static void main(String[] args) {
+        
+    }
+}
