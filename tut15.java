859d03facdef2e9e5b9e392f1e9f2e4653cdc22a
Kushagra <kushagravats911@gmail.com>
2023-11-04 00:25:00 +0530

    Chapter 3 : Pracrice Set

---
+++ tut15.java
@@ -0,0 +1,46 @@
+// Convert a string in lowercase 
+// public class tut15 {
+//     public static void main(String[] args) {
+//         String str1="Kushagra";
+//         String str2=new String("Vatsa");
+//         System.out.println("String in lowercase are "+str1.toLowerCase()+" and "+str2.toLowerCase());
+//     }
+// }
+
+//To replace spaces with underscore
+// public class tut15 {
+
+//     public static void main(String[] args) {
+//         String str1="Kushagra Vatsa";
+//         System.out.println("String after replacing space with underscore is "+str1.replace(' ', '_'));
+
+//     }
+// }
+
+//
+
+//To replace <|Name|> with Kushagra in the following string
+//Str="Dear <|name| , Thanks a lot>"
+
+// public class tut15 {
+
+//     public static void main(String[] args) {
+//         String str="Dear <|Name|>,Thanks a lot! ";
+//         System.out.println("String after replacing <|Name is|> "+str.replace("<|Name|>", "Kushagra"));
+//     }
+// }
+
+//WAP to detect double and triple spaces in a string
+/**
+ * tut15
+ */
+// public class tut15 {
+
+//     public static void main(String[] args) {
+//         String str1="This string contains  double and   triple spaces";
+//         System.out.println("Index of double spaces "+str1.indexOf("  "));
+//         System.out.println("Index of triple spaces "+str1.indexOf("   "));
+//         System.out.println("Index of four spaces "+str1.indexOf("    "));//It returns -1 since it is not presemt in the string 
+//     }
+//}
+
