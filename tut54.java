6851e3fa3a1a1eb0d92acdf677270920c7c825a3
Kushagra <kushagravats911@gmail.com>
2023-11-21 21:17:06 +0530

    Interface

---
+++ tut54.java
@@ -0,0 +1,10 @@
+/*
+ * Interface:
+ * English-It is a point where 2 systems meet or interact
+ * Jave-It is a group of related methods with empty bodies.
+ * 
+ * The advantages of using interfaces in Java are as follows:
+ * Without bothering about the implementation part, we can achieve the security of the implementation.
+ * In Java, multiple inheritances are not allowed, however, you can use an interface to make use of it as you can implement more than one interface.
+ */
+
